dfc352301dcdabe627b3f3936ddcf61ddf383159
HariomSaini <hariomsaini0011@gmail.com>
2019-06-12 15:16:52 +0530

    Changes in node app

--- nodetestapp/index.js
+++ nodetestapp/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const redis = require('redis')
+const process = require('process')
 const app = express();
 
-const app = express();
-const client = redis.createClient();
+// const app = express();
+const client = redis.createClient( {
+host: 'redis-server',
+port: 6379
+} ) ;
 client.set('visits', 0)
 
 
 app.get('/', (req, res) => {
+  process.exit(0);
 //   res.send('How are you doing');
   client.get('visits',(error, visits))
   res.send('No. of visits is '+ visits)
   client.set('visits', parseInt(visits)+1)
 }); 
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
+app.listen(8081, () => {
+  console.log('Listening on port 8081');
 });
