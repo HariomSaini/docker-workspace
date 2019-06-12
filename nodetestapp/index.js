87a4af3ba0cdfd9c35e4157957ce44fecf037659
HariomSaini <hariomsaini0011@gmail.com>
2019-06-12 10:55:26 +0530

    Added more docker mania

--- nodetestapp/index.js
+++ nodetestapp/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const redis = require('redis')
 const app = express();
 
+const app = express();
+const client = redis.createClient();
+client.set('visits', 0)
+
+
 app.get('/', (req, res) => {
-  res.send('How are you doing');
-});
+//   res.send('How are you doing');
+  client.get('visits',(error, visits))
+  res.send('No. of visits is '+ visits)
+  client.set('visits', parseInt(visits)+1)
+}); 
 
 app.listen(8080, () => {
   console.log('Listening on port 8080');
